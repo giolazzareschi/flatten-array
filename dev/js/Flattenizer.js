247602597f3facac7941e6427ae197bad9f393ed
Giordano Bruno Lazzareschi <giolazzareschi@gmail.com>
2015-08-14 23:04:58 -0300

    version 3

--- dev/js/Flattenizer.js
+++ dev/js/Flattenizer.js
@@ -8,17 +8,23 @@
 		return arr ? recursive.bind( this, arr )() : [];
 	};
 
+	/*
+		add:  adds values to the buffer
+	*/
 	function add( vl ){
 		this.buffer.push( vl );
 	};
 
+	/*
+		recursive:  starts the recursive operation over the initial nested array to be flattened
+	*/
 	function recursive( arr ){		
 		for( var p in arr ){ 
-			var md = arr[ p ];
-			if( !Array.isArray( md.length ) )
+			var md = arr[ p ]; //gets the current value
+			if( !Array.isArray( md ) ) // validates if it's an array
 				add.apply( this, [md] );
 
-			recursive.apply( this, [md] );
+			recursive.apply( this, [md] ); //recurse it
 		}
 		return this.buffer;
 	}
@@ -28,3 +34,7 @@
 		flatten : this.flatten
 	};
 });
+
+var flattenizer = new Flattenizer(); //initialize an object
+finalflatten = flattenizer.flatten([2,3,2,3,[2,3,[2,4],[3]],3]); //call the method
+console.log( finalflatten ); //output the result
